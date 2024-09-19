6d480e6073f73b437d33234dc53e732a5657fb56
tyczof <tyczka.zosia@gmail.com>
2024-09-19 13:14:44 +0200

    Fixed incompatible column name

---
+++ HotDesks/Migrations/20240919111255_RenameIsCancelledColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotDesks.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameIsCancelledColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isCanceled",
+                table: "Reservations",
+                newName: "isCancelled");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isCancelled",
+                table: "Reservations",
+                newName: "isCanceled");
+        }
+    }
+}
